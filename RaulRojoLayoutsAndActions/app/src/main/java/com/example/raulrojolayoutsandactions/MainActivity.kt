2b10decafd39fa3f5d3ef527eb9dd0c2ce478bd5
raulrojomunoz <raulrojom15@gmail.com>
2026-05-13 14:25:15 +0200

    Add files via upload

--- RaulRojoLayoutsAndActions/app/src/main/java/com/example/raulrojolayoutsandactions/MainActivity.kt
+++ RaulRojoLayoutsAndActions/app/src/main/java/com/example/raulrojolayoutsandactions/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.raulrojolayoutsandactions
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.raulrojolayoutsandactions.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+//    private lateinit var binding:
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
-        setContentView(R.layout.layout_calculator)
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-        //    val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-        //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-        //   insets
-       // }
+        enableEdgeToEdge()
+
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+
+        setContentView(R.layout.activity_actions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.action)){ v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setWriteToLogCatButtonListener()
+    }
+
+    private fun setWriteToLogCatButtonListener() {
+        binding.   .setOnClickListener {
+            Log.i("MyAppList", "Message From the listener")
+        }
+    }
+
+    fun writeToLogCat(view: View) {
+        Log.i("MyApp","Message from my App")
     }
 }
